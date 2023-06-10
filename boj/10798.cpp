6e80a63cd99e8079c3b89cbd99cb60b1e1c3461e
김현욱 <company@wiselight.kr>
2023-06-10 16:24:35 +0900

    BOJ 10798

---
+++ boj/10798.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+char arr[5][16];
+
+int main() {
+	for(int i = 0; i < 5; i++) cin >> arr[i];
+	
+	for(int i = 0; i < 15; i++) {
+		for(int j = 0; j < 5; j++) {
+			if(arr[j][i] != '\0')
+				 cout << arr[j][i];
+		}
+	}
+	return 0;
+}
