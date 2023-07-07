0f84418c2df0512aa9de30588a8c4efa6ab2268d
김현욱 <company@wiselight.kr>
2023-07-07 14:13:46 +0900

    BOJ 11866

---
+++ 11866.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	queue<int> q;
+	
+	for(int i = 1; i <= n; i++)
+		q.push(i);
+	
+	cout << "<";
+	while(!q.empty()) {
+		for(int i = 0; i < k - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		
+		cout << q.front();
+		if(q.size() > 1)
+			cout << ", ";
+		q.pop();
+	}
+	
+	cout << ">";
+	
+	return 0;
+}
